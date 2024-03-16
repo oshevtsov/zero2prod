563578f96a567b532f4870b21aba1f7ad0cfd89e
Oleksii Shevtsov <alex.shevtsov1988@gmail.com>
2024-03-16 13:28:33 +0100

    Refactor health check integration test

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,17 +1,23 @@
-fn spawn_app() -> Result<(), std::io::Error> {
-    let server = zero2prod::run()?;
+fn spawn_app() -> Result<String, std::io::Error> {
+    // Port 0 is special-cased at the OS level: trying to bind port 0 will trigger an OS scan for
+    // an available port which will then be bound to the application.
+    let listener = std::net::TcpListener::bind("127.0.0.1:0")?;
+    let port = listener.local_addr()?.port();
+    let server = zero2prod::run(listener)?;
     tokio::spawn(server);
-    Ok(())
+    Ok(format!("http://127.0.0.1:{}", port))
 }
 
 #[tokio::test]
 async fn health_check_works() {
-    spawn_app().expect("Failed to spawn the app.");
+    // Tokio runtime will kill the background task when the test finishes
+    let address = spawn_app().expect("Failed to spawn the app.");
 
     let client = reqwest::Client::new();
 
+    let url = format!("{}/health_check", address);
     let respose = client
-        .get("http://127.0.0.1:8000/health_check")
+        .get(url)
         .send()
         .await
         .expect("Failed to execute health check request");
